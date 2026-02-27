0399ab4fa47ebbb9f9af90a8a3a969889da5e126
Michael K <35264484+Mkassabov@users.noreply.github.com>
2026-02-26 19:43:13 -0500

    chore: fix aws test running to only call `fromNodeProviderChain` once (#104)

--- distilled-aws/vitest.config.ts
+++ distilled-aws/vitest.config.ts
@@ -6,6 +6,8 @@ import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    globalSetup: ["./test/global-setup.ts"],
+    setupFiles: ["./test/setup.ts"],
     include: ["test/**/*.test.ts", "scripts/**/*.test.ts"],
     exclude: [
       ...configDefaults.exclude,
